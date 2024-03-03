4fb23f69e99e38ea369192c7104c1867f693ed11
Hirofumi Tsuzuki <hrfm.tsuzuki@gmail.com>
2024-03-03 13:54:02 +0900

    add logging and error handling

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import json
+import logging
 import os
 import subprocess
-from subprocess import PIPE
+import sys
+from subprocess import PIPE, TimeoutExpired
 
 import ambient
+
+logging.basicConfig(format="[%(levelname)s] %(message)s", level=logging.DEBUG)
 
 
 def main():
@@ -12,23 +16,43 @@ def main():
     speedtest_server = os.environ.get("SPEEDTEST_SERVER", "21569")
 
     command = f"speedtest -s {speedtest_server} -f json"
-    proc = subprocess.run(
-        command,
-        shell=True,
-        stdout=PIPE,
-        stderr=PIPE,
-        text=True,
-        timeout=30,
-    )
-    result = json.loads(proc.stdout)
+    try:
+        proc = subprocess.run(
+            command,
+            shell=True,
+            stdout=PIPE,
+            stderr=PIPE,
+            text=True,
+            timeout=30,
+        )
+    except TimeoutExpired:
+        logging.error("Timeout Expired: 30 (sec)")
+        sys.exit(1)
 
-    am = ambient.Ambient(ambient_channel_id, ambient_write_key)
-    am.send(
-        {
-            "d1": result["download"]["bandwidth"] / (1000**2 / 8),
-            "d2": result["upload"]["bandwidth"] / (1000**2 / 8),
-        }
-    )
+    if proc.returncode == 0:
+        result = json.loads(proc.stdout)
+
+        logging.info(
+            f"Server: {result['server']['name']} ({result['server']['country']}, {result['server']['location']})"  # noqa: E501
+        )
+
+        download = result["download"]["bandwidth"] / (1000**2 / 8)
+        upload = result["upload"]["bandwidth"] / (1000**2 / 8)
+
+        logging.info(f"Download: {download} (Mbps)")
+        logging.info(f"Upload: {upload} (Mbps)")
+
+        am = ambient.Ambient(ambient_channel_id, ambient_write_key)
+        am.send(
+            {
+                "d1": download,
+                "d2": upload,
+            }
+        )
+    else:
+        logging.error(f"return code: {proc.returncode}")
+        logging.error(f"stderr: {proc.stderr}")
+        sys.exit(proc.returncode)
 
 
 if __name__ == "__main__":
